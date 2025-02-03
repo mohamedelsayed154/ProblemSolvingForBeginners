703360aaba8bd9a6e7d1314f4f5dcc2eeb7f2210
Mohamed Ahmed <117952210+mohamedelsayed154@users.noreply.github.com>
2025-02-04 00:50:06 +0200

    Create Problem#1.cpp
    Multipliaction Table From 1 to 10

---
+++ Problem#1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void PrintHeader();
+
+int main() {
+	PrintHeader();
+	for (int i = 1; i <= 10; i++) {
+
+		if (i < 10) {
+			cout << i << "   |";
+		}
+		else {
+			cout << i << "  |";
+		}
+		 for (int j = 1; j <= 10; j++) {
+			 
+			 if (j * i < 10) {
+				 cout <<  j * i << "  ";
+			 }
+			 else {
+				 cout << j * i << " ";
+			 }
+		} 
+		
+		cout << "\n";
+	}
+}
+
+void PrintHeader() {
+	cout << "     1  2  3  4  5  6  7  8  9  10\n";
+	cout << "----------------------------------\n";
+}
