8c40ef18e352979db6331c58d0d4785366840ab9
Mohamed Ahmed <117952210+mohamedelsayed154@users.noreply.github.com>
2025-02-04 06:23:17 +0200

    Create Problem#2.cpp

---
+++ Problem#2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int ReadNumber();
+void PrimeOrNot(int n);
+
+int main() {
+
+
+}
+
+int ReadNumber() {
+	int n;
+	cout << "Entre the number : ";
+	cin >> n;
+}
+
+void PrimeOrNot(int n) {
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 == 0) {
+			continue;
+		}
+		else {
+			cout << i << endl;
+		}
+	}
+}
