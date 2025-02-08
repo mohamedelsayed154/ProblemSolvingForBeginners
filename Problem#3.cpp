82bd4596be36f9fb04b8e1853053fe7683e919ed
Mohamed Ahmed <117952210+mohamedelsayed154@users.noreply.github.com>
2025-02-09 00:55:16 +0200

    Create Problem#3.cpp

---
+++ Problem#3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int ScanNumber();
+int CheckNumber(int n);
+void PerfectOrNot(int sum, int n);
+
+int main() {
+    int n = ScanNumber(); 
+    int sum = CheckNumber(n);
+    PerfectOrNot(sum, n);
+}
+
+int ScanNumber() {
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    return n;
+}
+
+int CheckNumber(int n) {
+    int sum = 0;
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0) { 
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+void PerfectOrNot(int sum, int n) {
+    if (n == sum) {
+        cout << n << " is perfect\n";
+    }
+    else {
+        cout << n << " is not perfect\n"; 
+    }
+}
