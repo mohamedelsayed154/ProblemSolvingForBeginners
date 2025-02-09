0ae570c70318df1d0906d8f08a583ccdce3d42b3
Mohamed Ahmed <117952210+mohamedelsayed154@users.noreply.github.com>
2025-02-09 14:38:08 +0200

    Create Problrm#6.cpp

---
+++ Problrm#6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int ValidNum();
+void SumDigits(int sum, int n);
+
+
+int main() {
+	int n = ValidNum();
+	int sum = 0;
+	SumDigits(sum, n);
+}
+
+int ValidNum() {
+	int n;
+	
+	do {
+		cout << "Entre the Num : ";
+		cin >> n;
+	} while (n < 0);
+	return n;
+}
+
+void SumDigits(int sum, int n) {
+	while (n > 0) {
+		sum = sum + ( n % 10);
+		n /= 10;
+	}
+	cout << "The Sum of last digits : " << sum << endl;
+
+}
