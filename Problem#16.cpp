522498be3388bfebfbe14ac19e71ebe1417f7bc3
Mohamed Ahmed <117952210+mohamedelsayed154@users.noreply.github.com>
2025-02-13 00:54:05 +0200

    Create Problem#16.cpp

---
+++ Problem#16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	for (char i = 65; i <= 90; i++) {
+
+		for (char x = 65; x <= 90; x++) {
+
+			for (char j = 65; j <= 90; j++) {
+				cout << i << x << j << endl;
+			}
+		}
+	}
+	
+}
