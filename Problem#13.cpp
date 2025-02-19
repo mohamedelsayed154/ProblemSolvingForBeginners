7302b579a4846d312e69bcbbf131f8de72556c8d
Mohamed Ahmed <117952210+mohamedelsayed154@users.noreply.github.com>
2025-02-19 17:06:06 +0200

    Update Problem#13.cpp

--- Problem#13.cpp
+++ Problem#13.cpp
@@ -1,3 +1,18 @@
+/*
+Write a program to read a number and print a inverted pattern as follows ?
+
+input : 3                         input : 5
+
+output : 
+1                                 output :
+22                                1
+333                               22
+                                  333
+				  4444
+                                  55555
+*/
+
+
 #include <iostream>
 using namespace std;
 
